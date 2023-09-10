c510c81165ec39c7d85b4bd1a882f3a3d1c9d46a
khoaleq98 <khoaleq.98@gmail.com>
2023-09-10 10:55:09 +0700

    update

---
+++ api/customer.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const router = express.Router();
+const  credentials = require('./credentials.json')
+const { google } = require("googleapis");
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+
+const readData = async () => {
+
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+  const spreadsheetId = "16TYKzK1K4y-zZInyxBzGmzKXao0bPYBNbJ8qH5zbiPs"; //read
+
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+  const sheetName = metaData.data.properties.title;
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+  });
+  return {
+    rows: getRows.data.values,
+    sheetName,
+  };
+}
+
+const authClient = async() => {
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+  return {
+    client,
+    googleSheets,
+    auth
+  }
+}
+
+router.post("/", async (req, res) => {
+  const {
+    client,
+    googleSheets,
+    auth
+  }= await authClient();
+  try {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    const { phone, name, company, email } = req.body;
+    // Write row(s) to spreadsheet
+    const spreadsheetId =  '1cpoR986WLMyMRhPsYuGvZ3J9q7oYZmiGBNspci428DE' // write;
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: "Sheet1",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [[ name , company , email, phone]],
+      },
+    });
+
+    res.json({
+      message: "Successfully submitted! Thank you!"
+    })
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+
+
+
+module.exports = (router);
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const customer = require("./api/customer");
 
 app.use(express.json({ extended: false }));
 
 app.use("/api/sheet-qr", product);
+app.use("/api/customer-info", customer);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port http://localhost:${PORT}`));
