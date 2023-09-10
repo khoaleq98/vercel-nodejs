430372cdce92257f95c247540fcb88a82cfad09b
khoaleq98 <khoaleq.98@gmail.com>
2023-09-10 11:22:35 +0700

    ud

--- api/product.js
+++ api/product.js
@@ -3,23 +3,6 @@ const router = express.Router();
 const  credentials = require('./credentials.json')
 const { google } = require("googleapis");
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
 /**
  * GET product list.
  *
@@ -27,7 +10,6 @@ const allowCors = fn => async (req, res) => {
  */
 
 const readData = async () => {
-
   const auth = new google.auth.GoogleAuth({
     credentials,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
